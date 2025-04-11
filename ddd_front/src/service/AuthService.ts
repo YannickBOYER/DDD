289e19f85d0ffb78d7b299f1956f45e25e76bf24
robin peignet <robin.peignet.contact@gmail.com>
2025-04-11 10:48:27 +0200

    Authentication page

---
+++ ddd_front/src/service/AuthService.ts
@@ -0,0 +1,28 @@
+// src/services/authService.js
+export async function login(username: String, password: String) {
+    try {
+        const response = await fetch('http://localhost:8000/api/auth/login/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+
+        if (!response.ok) {
+            throw new Error('Échec de la requête API')
+        }
+
+        const data = await response.json()
+        localStorage.setItem('token', data['token'])
+    } catch (error) {
+        console.error('Erreur lors de l\'appel API:', error)
+    }
+}
+
+export function logout() {
+
+}
---
+++ ddd_front/src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="login-page">
+      <h2>Connexion</h2>
+      <form @submit.prevent="login">
+        <input v-model="username" type="username" placeholder="Nom d'utilisateur" />
+        <input v-model="password" type="password" placeholder="Mot de passe" />
+        <button type="submit">Se connecter</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import { login } from '@/service/AuthService'
+  export default{
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+    methods:{
+        async login(){
+            console.log(this.username)
+            if(this.username && this.password){
+                await login(this.username, this.password);
+            }
+        }
+    }
+  }
+//   export default {
+//     data() {
+//       return {
+//         email: '',
+//         password: ''
+//       }
+//     },
+//     methods: {
+//       login() {
+//         login()
+//         // Authentification fictive
+//         /*if (this.email && this.password) {
+//           localStorage.setItem('auth', true)
+//           this.$router.push('/dashboard')
+//         }*/
+//       }
+//     }
+//   }
+  </script>
