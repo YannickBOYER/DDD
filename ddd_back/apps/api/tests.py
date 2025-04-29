598f081257c8b1fd699af2efe8acf597d6766791
robin peignet <robin.peignet.contact@gmail.com>
2025-04-29 17:56:20 +0200

    test

--- ddd_back/apps/api/tests.py
+++ ddd_back/apps/api/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class LoginEndpointTest(APITestCase):
+    def setUp(self):
+        self.username = "analyst"
+        self.password = "analyst"
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.client = APIClient()
+        self.url = 'http://localhost:8000/api/auth/login/'
+
+    def test_login_success(self):
+        data = {
+            "username": self.username,
+            "password": self.password
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("token", response.data)
+
+    def test_login_fail(self):
+        data = {
+            "username": self.username,
+            "password": "mauvais_mot_de_passe"
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
